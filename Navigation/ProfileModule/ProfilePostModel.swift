2c6439b99dde46377c09c415da24dfbba4fc111b
bolyaolya <1586446@mail.ru>
2023-04-18 19:37:52 +0300

    Создан таргет StorageService, создана схема Release

--- Navigation/ProfileModule/ProfilePostModel.swift
+++ Navigation/ProfileModule/ProfilePostModel.swift
@@ -9,18 +9,18 @@ import UIKit
 
     //свойства
 
-    public struct Post {
-        let author : String
-        let description: String
-        let image: String
-        var likes: Int
-        var views: Int
-    }
-
-
-    public var post : [Post] = [
-        Post(author: "Стивен Кинг", description: "11/22/63", image: "112263", likes: 3048, views: 5093),
-        Post(author: "Михаил Булгаков", description: "Мастер и Маргарита", image: "master", likes: 2048, views: 4809),
-        Post(author: "Фредерик Бакман", description: "Тревожные люди", image: "trevozhnye", likes: 1033, views: 2048),
-        Post(author: "Дэн Браун", description: "Код да Винчи", image: "daVinci", likes: 2567, views: 3493)
-    ]
+//    public struct Post {
+//        let author : String
+//        let description: String
+//        let image: String
+//        var likes: Int
+//        var views: Int
+//    }
+//
+//
+//    public var post : [Post] = [
+//        Post(author: "Стивен Кинг", description: "11/22/63", image: "112263", likes: 3048, views: 5093),
+//        Post(author: "Михаил Булгаков", description: "Мастер и Маргарита", image: "master", likes: 2048, views: 4809),
+//        Post(author: "Фредерик Бакман", description: "Тревожные люди", image: "trevozhnye", likes: 1033, views: 2048),
+//        Post(author: "Дэн Браун", description: "Код да Винчи", image: "daVinci", likes: 2567, views: 3493)
+//    ]
